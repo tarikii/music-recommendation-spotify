ba7090d681b91973c360a1fd9962f201cb2381a9
tarikii <tarikaabouchazougarh@gmail.com>
2024-01-10 22:32:40 +0100

    Error Handling and API interaction! End

---
+++ README.md
@@ -0,0 +1,47 @@
+# Spotify Search Music
+
+Welcome to Spotify Search Music! This Python script allows you to search for artists, view their top tracks, get characteristics of a track, and search for tracks in a playlist using the Spotify API.
+
+## Getting Started
+
+1. **Install Dependencies:**
+   ```bash
+   pip install -r requirements.txt
+
+    Set Up Spotify API Credentials:
+        Create a .env file in the project root.
+        Add the following lines to the .env file:
+
+        dotenv
+
+    CLIENT_ID=your_spotify_client_id
+    CLIENT_SECRET=your_spotify_client_secret
+
+## Usage
+
+    Choose an option from the menu:
+        1. Search for an artist
+        2. Show top tracks of an artist
+        3. Get characteristics of a track
+        4. Search for the tracks of a playlist
+        0. Exit
+
+    Follow the prompts to input relevant information, such as artist name, track URL, or playlist URL.
+
+    View the results and enjoy exploring music details!
+
+## Error Handling
+
+The script includes error handling for various scenarios, such as invalid input, invalid URLs, and errors during API requests.
+
+Feel free to customize and extend the script based on your requirements!
+
+    Note: Replace your_spotify_client_id and your_spotify_client_secret with your actual Spotify API credentials in the .env file.
+
+## License
+
+This project is licensed under the MIT License
+
+## Author
+
+Tarik Aabouch - Tarikii in github
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post, get
+from requests import post, get, exceptions
 import json
 
 load_dotenv()
@@ -9,6 +9,18 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+
+def show_menu():
+    print("WELCOME TO SPOTIFY SEARCH MUSIC!")
+    print()
+    print()
+    print("1. Search for an artist")
+    print("2. Show top tracks of an artist")
+    print("3. Get characteristics of a track")
+    print("4. Search for the tracks of a playlist")
+    print("0. Exit")
+    print()
+    return int(input("Choose your option: "))
 
 def get_token():
     auth_string = client_id + ":" + client_secret
@@ -35,6 +47,52 @@ def get_track_id(track):
     return track_id[4]
 
 
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+
+    try:
+        result.raise_for_status()
+        json_result = result.json()["artists"]["items"]
+
+        if len(json_result) == 0:
+            print()
+            print(f"No artist found with the name: {artist_name}")
+            print()
+            return None
+
+        return json_result[0]
+    except exceptions.HTTPError as errh:
+        print(f"HTTP Error: {errh}")
+        return None
+    except exceptions.RequestException as err:
+        print(f"Error during the request: {err}")
+        return None
+    except ValueError as ve:
+        print(f"Error parsing JSON response: {ve}")
+        return None
+
+
+
+def get_artist_songs(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+
+    if "tracks" in json_result:
+        return json_result["tracks"]
+    else:
+        print("Error: 'tracks' not found in the API response.")
+        return None
+    
+
 def get_characteristics_track(token, track_id, keys):
     url = f"https://api.spotify.com/v1/audio-features/{track_id}"
     headers = get_auth_header(token)
@@ -56,38 +114,28 @@ def get_characteristics_track(token, track_id, keys):
         # Print the error response content
         print(f"Error: {response.status_code}")
         return None
-
-
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-
-    headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"
-
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-
-    if len(json_result) == 0:
-        print("There is not an artist with this name on Spotify :(")
-        return None
     
-    return json_result[0]
 
-
-def get_artist_songs(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+def get_playlist_tracks_names(token, playlist_id):
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
     headers = get_auth_header(token)
 
     result = get(url, headers=headers)
     json_result = json.loads(result.content)
 
+    if "name" in json_result:
+        playlist_name = json_result['name']
+    else:
+        print("Error: 'name' not found in the API response.")
+        return None, None
+
     if "tracks" in json_result:
-        return json_result["tracks"]
+        tracks_info = json_result['tracks']['items']
+        track_names = [track['track']['name'] for track in tracks_info]
+        return playlist_name, track_names
     else:
         print("Error: 'tracks' not found in the API response.")
-        return None
-
+        return None, None
 
 
 def main():
@@ -95,14 +143,99 @@ def main():
     track_url = "https://open.spotify.com/track/0Y2i84QWPFiFHQfEQDgHya?si=7918f849eee04290"
     track_id = get_track_id(track_url)
     desired_keys = ["danceability", "energy", "loudness", "tempo", "valence", "speechiness"]
-    artist = search_for_artist(token, "Leviathan")
-    artist_id = artist["id"]
-    songs = get_artist_songs(token, artist_id)
+    #artist = search_for_artist(token, "Leviathan")
+    #artist_id = artist["id"]
+    #songs = get_artist_songs(token, artist_id)
 
-    #for idx, song in enumerate(songs):
-        #print((idx + 1), song["name"])
+    while True:
+        try:
+            option = show_menu()
 
-    track_info = get_characteristics_track(token, track_id, desired_keys)
+            if option == 1:
+                try:
+                    artist_name = input("Name of the artist: ")
+                    artist = search_for_artist(token, artist_name)
+
+                    if artist:
+                        print(f"Artist found: {artist['name']} (Followers: {artist['followers']['total']})")
+                except ValueError:
+                    print()
+                    print("Invalid Input!")
+                    print()
+
+            
+            elif option == 2:
+                try:
+
+                    artist_name = input("Name of the artist: ")
+                    artist = search_for_artist(token, artist_name)
+                    artist_id = artist["id"]
+                    top_songs = get_artist_songs(token, artist_id)
+
+                    for idx, song in enumerate(top_songs):
+                        print((idx + 1), song["name"])
+                
+                except TypeError:
+                    pass
+                
+            
+            elif option == 3:
+                try:
+
+                    track_url = input("Paste here the track URL of Spotify: ")
+                    track_id = get_track_id(track_url)
+
+                    track_info = get_characteristics_track(token, track_id, desired_keys)
+
+                    for idx, info in track_info.items():
+                        print((idx + 1), info)
+                
+                except IndexError:
+                    print()
+                    print("Not a valid URL!")
+                    print()
+
+                except ValueError:
+                    print()
+                    print("Not a valid URL!")
+                    print()
+            
+
+            elif option == 4:
+                try:
+
+                    playlist_url = input("Paste here the URL of the Spotify playlist: ")
+                    playlist_id = get_track_id(playlist_url)
+                    playlist_name, track_names = get_playlist_tracks_names(token, playlist_id)
+
+                    if track_names:
+                        print(f"Tracks in the playlist '{playlist_name}':")
+                        for idx, track_name in enumerate(track_names):
+                            print(f"{idx + 1}. {track_name}")
+                    else:
+                        print("Failed to retrieve playlist track names.")
+                except IndexError:
+                    print()
+                    print("Not a valid URL!")
+                    print()
+                
+                except ValueError:
+                    print()
+                    print("Not a valid URL!")
+                    print()
+            
+            elif option == 0:
+                return False
+            
+
+            else:
+                print()
+                print("Invalid option! Pick between the numbers 0-4")
+            
+        except ValueError:
+            print()
+            print("Provide a number, not a string!")
+            print()
 
 if __name__ == "__main__":
     main()
